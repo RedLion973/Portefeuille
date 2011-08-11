6dccc1409f76c61b31c393dd6f4c4e3654d49d1e
Ludovic LEGENDART <red_lion-973@hotmail.fr>
2011-08-11 15:15:30 +0200

    update planning

--- fut/views.py
+++ fut/views.py
@@ -1,8 +1,7 @@
+import datetime
 from django.views.generic import DetailView, ListView
 from FUTFactory.fut.models import FUT
 from FUTFactory.edm.models import Folder
-
-
 
 class FUTListView(ListView):
     queryset = FUT.objects.order_by('state__phase')
@@ -32,7 +31,22 @@ class FUTDetailView(DetailView):
         context = super(DetailView, self).get_context_data(**kwargs)
         space = self.object.sharing_doc_space
         tree = self.get_folders(space.id)
+        sd = 0
+        ed = 0
+        sd_no_weekend = 0
+        ed_no_weekend = 0
+        if self.object.has_planning:
+            s_daydiff = self.object.scheduled_end_date.weekday() - self.object.scheduled_start_date.weekday()
+            sd_no_weekend = ((self.object.scheduled_end_date - self.object.scheduled_start_date).days - s_daydiff) / 7 * 5 + min(s_daydiff, 5)
+            e_daydiff = self.object.effective_end_date.weekday() - self.object.effective_start_date.weekday()
+            ed_no_weekend = ((self.object.effective_end_date - self.object.effective_start_date).days - e_daydiff) / 7 * 5 + min(e_daydiff, 5)
+            ed = (self.object.effective_end_date - self.object.effective_start_date).days
+            sd = (self.object.scheduled_end_date - self.object.scheduled_start_date).days
         context.update({
-            'tree': tree
+            'tree': tree,
+            'scheduled_days': sd,
+            'effective_days': ed,
+            'scheduled_days_nw': sd_no_weekend,
+            'effective_days_nw': ed_no_weekend
         })
         return context

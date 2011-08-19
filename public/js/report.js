f1c4e3ecb6b3b8002f56cb2d12f71846a8d7cd42
Ludovic LEGENDART <red_lion-973@hotmail.com>
2011-08-20 00:38:44 +0200

    improves report

--- public/js/report.js
+++ public/js/report.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	$("#id_scheduled_start_date_0").datepicker();
-	$("#id_scheduled_start_date_1").datepicker();
-	$("#id_scheduled_end_date_0").datepicker();
-	$("#id_scheduled_end_date_1").datepicker();
-	$("#id_effective_start_date_0").datepicker();
-	$("#id_effective_start_date_1").datepicker();
-	$("#id_effective_end_date_0").datepicker();
-	$("#id_effective_end_date_1").datepicker();
+	$("#id_scheduled_start_date_0").datepicker({ dateFormat: 'dd/mm/yy' });
+	$("#id_scheduled_start_date_1").datepicker({ dateFormat: 'dd/mm/yy' });
+	$("#id_scheduled_end_date_0").datepicker({ dateFormat: 'dd/mm/yy' });
+	$("#id_scheduled_end_date_1").datepicker({ dateFormat: 'dd/mm/yy' });
+	$("#id_effective_start_date_0").datepicker({ dateFormat: 'dd/mm/yy' });
+	$("#id_effective_start_date_1").datepicker({ dateFormat: 'dd/mm/yy' });
+	$("#id_effective_end_date_0").datepicker({ dateFormat: 'dd/mm/yy' });
+	$("#id_effective_end_date_1").datepicker({ dateFormat: 'dd/mm/yy' });
 	$('#reset').click(function() {
 		$(":input").val('');
 		$('#filter_form').submit();
